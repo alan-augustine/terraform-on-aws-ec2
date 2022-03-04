0b69b07197e6a77a91ecc01596a007d9e397aa1f
alan-augustine <alaaugustine007@gmail.com>
2022-03-04 20:46:46 +0530

    section 4 changes for latest version

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
@@ -1,11 +1,11 @@
-# EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = data.aws_ami.amzlinux2.id
-  instance_type = var.instance_type
-  user_data = file("${path.module}/app1-install.sh")
-  key_name = var.instance_keypair
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
-  tags = {
-    "Name" = "EC2 Demo 2"
-  }
+    ami = data.aws_ami.amzlinux2.id
+    instance_type = var.instance_type
+    user_data = file("${path.module}/app1-install.sh")
+    key_name = var.instance_keypair
+    security_groups = [ aws_security_group.vpc-ssh.name , aws_security_group.vpc-web.name ]
+    tags = {
+        Name = "Hello World"
+    }
+  
 }
--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c6-outputs.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c6-outputs.tf
@@ -1,13 +1,9 @@
-# Terraform Output Values
-
-# EC2 Instance Public IP
 output "instance_publicip" {
-  description = "EC2 Instance Public IP"
   value = aws_instance.myec2vm.public_ip
+  description = "EC2 Instance public IP"
 }
 
-# EC2 Instance Public DNS
 output "instance_publicdns" {
-  description = "EC2 Instance Public DNS"
   value = aws_instance.myec2vm.public_dns
+  description = "EC2 Instance public DNS"
 }
