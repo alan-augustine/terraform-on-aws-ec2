7c7100fb499fd3bf6dfe167d823bdc3b02764e51
alan-augustine <alaaugustine007@gmail.com>
2022-03-19 11:05:17 +0530

    Section 6 finished

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c3-vpc.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c3-vpc.tf
@@ -1,53 +1,45 @@
-# Create VPC Terraform Module
+# we can give whatever name we like eg: module "myvpc" {...}
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.78.0"  
-  # version = "~> 2.78"
+  version = "3.13.0"
 
-  # VPC Basic Details
+  # VPC Basic details
   name = "vpc-dev"
-  cidr = "10.0.0.0/16"   
-  azs                 = ["us-east-1a", "us-east-1b"]
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  cidr = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  # Database Subnets
-  create_database_subnet_group = true
-  create_database_subnet_route_table= true
-  database_subnets    = ["10.0.151.0/24", "10.0.152.0/24"]
+  # Database subnets
+  create_database_subnet_group = true 
+  database_subnets = ["10.0.151.0/24", "10.0.152.0/24"]
+  # create_database_nat_gateway_route = false
+  # create_database_internet_gateway_route = false
 
-  #create_database_nat_gateway_route = true
-  #create_database_internet_gateway_route = true
-
-  # NAT Gateways - Outbound Communication
+  # NAT Gateway for outbound internet communication
   enable_nat_gateway = true
-  single_nat_gateway = true
+  # we just one NAT Gateway for all AZs, to reduce cost (not one NAT Gateway per AZ)
+  single_nat_gateway = true 
 
-  # VPC DNS Parameters
-  enable_dns_hostnames = true
-  enable_dns_support = true
+  # DNS parameters
+  enable_dns_hostnames = true 
+  enable_dns_support   = true
 
-  public_subnet_tags = {
-    Type = "public-subnets"
+  tags ={
+      Owner = "Alan"
+      Environment = "dev"
   }
-
-  private_subnet_tags = {
-    Type = "private-subnets"
-  }
-
-  database_subnet_tags = {
-    Type = "database-subnets"
-  }
-
-  tags = {
-    Owner = "kalyan"
-    Environment = "dev"
-  }
-
   vpc_tags = {
-    Name = "vpc-dev"
+      Name = "vpc-dev"
   }
+  private_subnet_tags = {
+      Name = "private-subnets"
+  }
+  public_subnet_tags = {
+      Name = "public-subnets"
+  }
+  database_subnet_tags = {
+      Name = "database-subnets"
+  }
+
 }
-
-
-
